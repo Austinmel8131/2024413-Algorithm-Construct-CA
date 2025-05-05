2b38febff3f6d51eccf31c63e2d1c89ed0e79061
Austinmel8131 <117985076+Austinmel8131@users.noreply.github.com>
2025-05-05 16:22:42 +0100

    3rd  Commit completed employee class

--- src/hospitalsystemproject/Employee.java
+++ src/hospitalsystemproject/Employee.java
@@ -9,4 +9,25 @@ package hospitalsystemproject;
 import java.util.Objects;
 
 public class Employee implements Comparable<Employee> {
+    private String name, employeeId, gender;
+    private Department department;
+
+    public Employee(String name, String employeeId, Department department, String gender) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.gender = gender;
+        this.department = department;
+        if (department != null) department.addEmployee(this);
+    }
+
+    public String getName() { return name; }
+    public String getEmployeeId() { return employeeId; }
+    public String getGender() { return gender; }
+    public Department getDepartment() { return department; }
+
+    public void setDepartment(Department department) {
+        if (this.department != null) this.department.removeEmployee(this);
+        this.department = department;
+        if (department != null) department.addEmployee(this);
+    }
 }
