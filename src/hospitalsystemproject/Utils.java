4418a97b1d6a3e93d6eaab0fbe1430ebef091126
Austinmel8131 <117985076+Austinmel8131@users.noreply.github.com>
2025-05-06 18:07:23 +0100

    7th Commit, fixed code to manage csv file for storing employee data generated or added to employee record

--- src/hospitalsystemproject/Utils.java
+++ src/hospitalsystemproject/Utils.java
@@ -12,6 +12,17 @@ import java.util.List;
  *
  * @author macbook
  */
-public class Utils {
+public class Utils {// Method to write users to a CSV file
+    public static void writeToCSV(List<Employee> employees, String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("Name,ID,Department,Gender,Role\n");
+            for (Employee e : employees) {
+                writer.write(e.toCsv() + "\n");
+            }
+            System.out.println("Saved to " + filename);
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+    }
 }
 
